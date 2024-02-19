ae0ebb1e44fcbcc115ffe13095a35ef906585510
RudaibaAdnin <1505032.ra@ugrad.cse.buet.ac.bd>
2024-02-19 02:30:10 -0500

    Javascript and ReactJs Assignment 3

---
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./index.css";
+import { modules } from "../../Database";
+import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaPlus } from "react-icons/fa";
+import { useParams } from "react-router";
+function ModuleList() {
+  const { courseId } = useParams();
+  const modulesList = modules.filter((module) => module.course === courseId);
+  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  return (
+    <>
+      {/* <!-- Add buttons here --> */}
+        <hr/>
+    
+        <span className="float-end">
+        <button type="button" className="btn btn-light border border-dark m-1"> Collapse All</button>
+        <button type="button" className="btn btn-light border border-dark m-1"> View Progress</button>
+        <select className="btn btn-light border border-dark">
+            <option>Publish All</option>
+            <option>Publish all items and modules</option>
+            <option>Unpublish</option>
+        </select>
+        <button type="button" className="btn btn-danger m-1"><FaPlus/> Module</button>
+        </span>
+
+         <br/>  <br/>  
+        <hr/>
+      <ul className="list-group wd-modules">
+        {modulesList.map((module) => (
+          <li
+            className="list-group-item"
+            onClick={() => setSelectedModule(module)}>
+            <div>
+              <FaEllipsisV className="me-2" />
+              {module.name}
+              <span className="float-end">
+                <FaCheckCircle className="text-success" />
+                <FaPlusCircle className="ms-2" />
+                <FaEllipsisV className="ms-2" />
+              </span>
+            </div>
+            {selectedModule._id === module._id && (
+              <ul className="list-group">
+                {module.lessons?.map((lesson) => (
+                  <li className="list-group-item">
+                    <FaEllipsisV className="me-2" />
+                    {lesson.name}
+                    <span className="float-end">
+                      <FaCheckCircle className="text-success" />
+                      <FaEllipsisV className="ms-2" />
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ModuleList;
---
+++ src/Kanbas/Courses/index.tsx
@@ -0,0 +1,47 @@
+import { courses} from "../../Kanbas/Database";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import { HiMiniBars3 } from "react-icons/hi2";
+import CourseNavigation from "./Navigation";
+import Modules from "./Modules";
+import Home from "./Home";
+import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
+import Grades from "./Grades";
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+
+function Courses() {
+  const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
+  const { pathname } = useLocation();
+  const { courseId } = useParams();
+  const course = courses.find((course) => course._id === courseId);
+  return (
+    <div>
+     {/* <h1 className="m-3" style={{ "color": "red", fontSize:20 }}><HiMiniBars3 /> Course {course?.name}  {links.map((link, index) =>(pathname.includes(link) ? " > "+link : ""))}</h1>  */}
+       <nav className="wd-breadcrumb">
+          <ol className="breadcrumb m-4">
+              <li className="breadcrumb-item" style={{ "color": "red", fontSize:20 }}><HiMiniBars3 /> Course {course?.name}</li>
+              <li className="breadcrumb-item active" style={{fontSize:20 }}>{links.map((link, index) =>(pathname.includes(link) ? link : ""))}</li>
+          </ol>
+        </nav>
+        
+         <CourseNavigation />
+        <div
+          className="overflow-y-scroll position-fixed bottom-0 end-0"
+          style={{ left: "320px", top: "50px" }} >
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home/>} />
+            <Route path="Modules" element={<Modules/>} />
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<Assignments/>} />
+            <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>} />
+            <Route path="Grades" element={<Grades/>} />
+          </Routes>
+        </div>
+      </div>
+  );
+}
+
+export default Courses
+
