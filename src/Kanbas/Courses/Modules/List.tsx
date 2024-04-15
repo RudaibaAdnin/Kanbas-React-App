ef0db9ef4fd98ec2946304c8836c2d6c3ded6bc0
Rudaiba Adnin <1505032.ra@ugrad.cse.buet.ac.bd>
2024-04-15 04:03:30 -0400

    Update List.tsx

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -30,14 +30,14 @@ function ModuleList() {
   };
 
   const handleAddModule = () => {
-    client.createModule(courseId, module).then((module) => {dispatch(addModule(module)); });
+    client.createModule(courseId, module);
+    dispatch(addModule(module));
     navigate(0);
   };
 
   const handleDeleteModule = (moduleId: string) => {
-    client.deleteModule(moduleId).then((status) => {
+      client.deleteModule(moduleId);
       dispatch(deleteModule(moduleId));
-    });
   };
 
 
